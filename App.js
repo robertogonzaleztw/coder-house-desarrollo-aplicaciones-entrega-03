f71aac2b8b1cc9f1d0d66a589cb48258a2135eeb
Roberto Gonzalez <ragonzalezm.19@gmail.com>
2023-01-25 00:27:22 -0300

    add logic

--- App.js
+++ App.js
@@ -1,12 +1,55 @@
+import React, { useState } from 'react';
+import { useFonts } from 'expo-font';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import NoteDetailScreen from './src/screens/NoteDetailScreen';
+import NotesScreen from './src/screens/NotesScreen';
+
+const PAGES = ['NotesScreen', 'NoteDetailScreen']
 
 export default function App() {
+  const [loaded] = useFonts({
+    InconsolataRegular: require("./src/assets/fonts/Inconsolata-Regular.ttf"),
+    InconsolataSemiBold: require("./src/assets/fonts/Inconsolata-SemiBold.ttf"),
+  })
+  const [notes, setNotes] = useState([{ title: 'nota 1', body: 'asdoaindoiqoweoiqweioqwe'}, {title: 'nota 2', body: 'ahlsnlkanckjasldknaslkdnl'}])
+  const [currentPage, setCurrenPage] = useState(PAGES[0])
+  const [noteSelected, setNoteSelected] = useState()
+
+  const onNoteSelectedHandler = (note) => {
+    setNoteSelected(note)
+    setCurrenPage(PAGES[1])
+  }
+
+  const onBackHandler= () => {
+    setNoteSelected(null)
+    setCurrenPage(PAGES[0])
+  }
+
+  let content
+
+  switch (currentPage) {
+    case 'NotesScreen':
+      content = <NotesScreen notes={notes} onNoteSelected={onNoteSelectedHandler} />
+      break
+    case 'NoteDetailScreen':
+      content = <NoteDetailScreen note={noteSelected} onBack={onBackHandler} />
+      break
+  }
+
+  if (!loaded) {
+    return null
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View>
+        <Text style={styles.title}>Notes App</Text>
+        <View style={styles.body}>
+          {content}
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
 
@@ -17,4 +60,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    fontFamily: 'InconsolataSemiBold'
+  }
 });
